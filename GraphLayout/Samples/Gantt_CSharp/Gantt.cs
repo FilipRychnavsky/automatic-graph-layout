6289e110caec75e25d7b0a251425e8da0c514248
filiprychnavsky <filip.r@email.cz>
2015-05-14 23:06:29 +0200

    sAS1 und 2

--- GraphLayout/Samples/Gantt_CSharp/Gantt.cs
+++ GraphLayout/Samples/Gantt_CSharp/Gantt.cs
@@ -40,22 +40,34 @@ namespace Gantt_CSharp
 		//TODO GViewerOnMouseMove
 		void CreateGraph()
 		{
-			Graph graph = new Graph("graph");
+			Graph rGraph_Drawing = new Graph("graph");
 			//graph.LayoutAlgorithmSettings=new MdsLayoutSettings();
 			gViewer.BackColor = System.Drawing.Color.FromArgb(10, System.Drawing.Color.Red);
 			ICurve rICurve_Rectangle = CurveFactory.CreateRectangle(10, 100, new Point());
 			//Node rNode_Start = new Node(rICurve_Rectangle, "Start");
 			//Microsoft.Msagl.Drawing.Node rNode_Drawing = new Microsoft.Msagl.Drawing.Node()
-			Microsoft.Msagl.Drawing.Node rNode_Drawing_Start = new Microsoft.Msagl.Drawing.Node("Start");
+			string sStart = "Start";
+			string sAS1 = "Arbeitsschritt 1";
+			string sAS2 = "Arbeitsschritt 2";
+			string sEnde = "Ende";
+			Microsoft.Msagl.Drawing.Node rNode_Drawing_Start = new Microsoft.Msagl.Drawing.Node(sStart);
+			Microsoft.Msagl.Drawing.Node rNode_Drawing_AS1 = new Microsoft.Msagl.Drawing.Node(sAS1);
+			Microsoft.Msagl.Drawing.Node rNode_Drawing_AS2 = new Microsoft.Msagl.Drawing.Node(sAS2);
+			Microsoft.Msagl.Drawing.Node rNode_Drawing_Ende = new Microsoft.Msagl.Drawing.Node(sEnde); 
+
+			rGraph_Drawing.AddEdge(sStart, sAS1);
+			rGraph_Drawing.AddEdge(sAS1, sAS2);
+			rGraph_Drawing.AddEdge(sAS2, sEnde);
+
 			rNode_Drawing_Start.Attr.Shape = Shape.Box;
 			//TODO Nodes from geometry - to set rectangle length and width
 			//TODO Some edges
 
-			graph.AddNode(rNode_Drawing_Start);
+			rGraph_Drawing.AddNode(rNode_Drawing_Start);
 			//TODO Orientationg - horizontal Left to Right
-			graph.LayoutAlgorithmSettings = new SugiyamaLayoutSettings();
-			gViewer.Graph = graph;
-			this.propertyGrid1.SelectedObject = graph;
+			rGraph_Drawing.LayoutAlgorithmSettings = new SugiyamaLayoutSettings();
+			gViewer.Graph = rGraph_Drawing;
+			this.propertyGrid1.SelectedObject = rGraph_Drawing;
 		}
 	}
 }
